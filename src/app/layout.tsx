e9cead846d3a0104e2af729de127eb7e554e3c67
FurqanNoor <82640979+FurqanNoor@users.noreply.github.com>
2024-12-12 18:14:18 +0200

    changes to layout.tsx
    added metadata in layout.tsx, changed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,53 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
+import { siteConfig } from "@/lib/site-config";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: ["Next.js", "React", "Tailwind CSS", "Framer Motion"],
+  authors: [
+    {
+      name: "Furqan Noor",
+      url: siteConfig.url,
+    },
+  ],
+  creator: "Furqan",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: siteConfig.twitterHandle,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export default function RootLayout({
   children,
@@ -13,6 +59,7 @@ export default function RootLayout({
       lang="en"
       className="bg-stone-950 container max-w-2xl mx-auto min-h-screen flex flex-col px-4 py-5"
     >
+      <head></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/lib/site-config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "Furgo",
+  url: "https://www.furqannoor.dev/",
+  description: "my portfolio website.",
+  ogImage: "https://www.myawesomewebsite.com/og-image.jpg",
+  twitterHandle: "@furggoo",
+};
